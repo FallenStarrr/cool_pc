c4ec623df3a31987932f6ae23e31db7d0ddcb72e
Vlad <vladfedorchencko@gmail.com>
2021-06-10 17:38:52 +0600

    Timer

---
+++ timer.js
@@ -0,0 +1,26 @@
+function timer() {
+
+
+    const countDate = new Date('June 20, 2021 00:00:00').getTime()
+    const now = new Date().getTime()
+    const gap = countDate - now
+    
+   const second = 1000
+   const minute = second * 60
+   const hour = minute * 60
+   const day = hour * 24
+
+   const days = Math.floor(gap / day)
+   const hours = Math.floor(gap % day  / hour)
+   const minutes = Math.floor(gap % hour / minute)
+   const seconds = Math.floor(gap % minute / second)
+   console.log(gap % minutes)
+  console.log(gap % second)
+   document.querySelector('.day').innerText = days
+   document.querySelector('.hour').innerText = hours
+   document.querySelector('.min').innerText = minutes
+   document.querySelector('.sec').innerText = seconds
+    
+}
+
+setInterval(timer, 1000)
